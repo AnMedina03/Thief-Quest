1c526d6a46b18da5c92e7ab345670b57e491090f
Andres J. Medina J <ajmedina_jacome@hotmail.com>
2023-07-13 16:32:34 -0500

    Actualización de página de inicio

--- html/indices-bitacora/videos-playtesting.js
+++ html/indices-bitacora/videos-playtesting.js
@@ -77,16 +77,16 @@ $('.playtesting-tri-p1').magnificPopup({
     }
 });
 
-// $('.playtesting-tri-p2').magnificPopup({
-//     items: {
-//         src: `
-//         <div class="fx-display popup-background">
-//             <iframe class="video-fullscreen" src="https://www.youtube.com/embed/TLOrT2_zbJc"
-//             title="Tutorial - Crear Tablero" frameborder="0"
-//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//             allowfullscreen></iframe>
-//         </div>
-//         `,
-//         type: 'inline'
-//     }
-// });
+$('.playtesting-tri-p2').magnificPopup({
+    items: {
+        src: `
+        <div class="fx-display popup-background">
+            <iframe class="video-fullscreen" src="https://www.youtube.com/embed/mnWLRLn3tiI"
+            title="Tutorial - Crear Tablero" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        </div>
+        `,
+        type: 'inline'
+    }
+});
