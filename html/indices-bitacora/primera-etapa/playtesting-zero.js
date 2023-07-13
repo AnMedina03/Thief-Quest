9987b1c19a2e2807fb1db39ff4a12e0ebb0a9847
Andres J. Medina J <ajmedina_jacome@hotmail.com>
2023-07-12 21:01:49 -0500

    Actualización de secciones de PlayTesting

--- html/indices-bitacora/primera-etapa/playtesting-zero.js
+++ html/indices-bitacora/primera-etapa/playtesting-zero.js
@@ -15,18 +15,17 @@ function puntoF(){
         <div class="container">
             <h3 class="text-white title-bg iguana-title">Elementos</h3>
             <div>
-            <div class="puntos-clave" style="padding: 1vh; margin: 1vh;>
-                <div class="linea">
-                    <p style="color:white;">Detalle de elementos usados:</p>
-                    <p style="color:lightblue;">
-                    Para el Playtesting Zero se empleó el siguiente tablero de juego con los siguientes elementos empleados en las fotos:
-                    </p>
-                </div>    
+            <div class="puntos-clave" style="padding: 1vh; margin: 1vh;">
+                <p style="color:white;">Detalle de elementos usados:</p>
+                <p style="color:lightblue;">
+                Para el Playtesting Zero se empleó el siguiente tablero de juego con los siguientes elementos empleados en las fotos:
+                </p>
             </div>
-            <div style="display: flex;justify-content: center; margin: 1vh;">
-                <img src="../img/pruebas/prueba1.jpeg" alt="Tablero v1" width="100" height="105">
+            <div class="fx-display fx-fil fx-wp gp-1">
+                <img src="../img/pruebas/playtesting-zero/Imagen2.png" alt="Manual v1" class="playtesting-element">
+                <img src="../img/pruebas/playtesting-zero/Imagen3.png" alt="Tablero v1" class="playtesting-element">
             </div>
-            <div class="puntos-clave" style="padding: 1vh; margin: 1vh;>
+            <div class="puntos-clave" style="padding: 1vh; margin: 1vh;">
                 <div class="linea">
                     <p style="color:lightblue;">
                     Se utilizó un tablero de juego formado por 6 horas A4, dicho tablero contenía la ubicación de las paradas ubicadas en los extremos del tablero,
@@ -40,43 +39,40 @@ function puntoF(){
             </div>
             <br>
             <h3 class="text-white title-bg iguana-title">Problemas Encontrados</h3>
-            <div class="puntos-clave" style="padding: 1vw;>
-                <div class="linea">
-                    <ul style="color:lightblue;">
-                        <h5 style="color:var(--prim-ligth);">Primera prueba</h5>
-                        <li>
-                            Con la primera versión de las reglas (antes de lanzarlas a pruebas con usuarios externos), los ladrones no comprendían claramente como llegar a su
-                            posición inicial. Estaban mal distribuidos y eran muy ambiguas.
-                        </li>
-                        <li>
-                            La movilidad de la iguana era "<b>AUTOMÁTICA</b>" pero existían problemas que eso ocurra porque no había algo o alguien específico que la moviera, resultando 
-                            en movimientos aleatorios que al final solo permitían que los ladrones pudieran escapar sin problemas y ganar, perdiendo así (la iguana) su relevancia en el juego.
-                        </li>
-                        <br>
-                        <h5 style="color:var(--prim-ligth);">Segunda prueba</h5>
-                        <li>
-                            Ya se tenían armadas las reglas y los personajes, pero aún esas reglas eran suceptibles a cambios debido a que eran demasiado ambigua, a tal punto que nuestro equipo
-                            no sabía como reaccionar a ciertos eventos.
-                        </li>
-                        <li>
-                            cómo el jugador de la iguana.
-                        </li>
-                        <br>
-                        <h5 style="color:var(--prim-ligth);">Segunda prueba</h5>
-                        <li>
-                            Con la primera versión de las reglas (antes de lanzarlas a pruebas con usuarios externos), los ladrones no comprendían claramente como llegar a su
-                            posición inicial. Estaban mal distribuidos y eran muy ambiguas.
-                        </li>
-                        <li>
-                            La movilidad de la iguana era "<b>AUTOMÁTICA</b>" pero existían problemas que eso ocurra porque no había algo o alguien específico que la moviera, resultando 
-                            en movimientos aleatorios que al final solo permitían que los ladrones pudieran escapar sin problemas y ganar, perdiendo así (la iguana) su relevancia en el juego.
-                        </li>
-                    </ul>
-                </div>    
+            <div class="fx-display fx-fil fx-wp gp-1">
+                <img src="../img/pruebas/playtesting-zero/playtesting0-prueba1.jpeg" alt="Manual v1" class="foto-playtesting">
+            </div>
+            <div class="puntos-clave"  style="padding: 1vh; margin: 1vh;">
+                <h5 style="color:var(--prim-ligth);">Primera prueba</h5>
+                <li style="color:lightblue;">
+                    Con la primera versión de las reglas (antes de lanzarlas a pruebas con usuarios externos), los ladrones no comprendían claramente como llegar a su
+                    posición inicial. Estaban mal distribuidos y eran muy ambiguas.
+                </li>
+                <li style="color:lightblue;">
+                    La movilidad de la iguana era "<b>AUTOMÁTICA</b>" pero existían problemas que eso ocurra porque no había algo o alguien específico que la moviera, resultando 
+                    en movimientos aleatorios que al final solo permitían que los ladrones pudieran escapar sin problemas y ganar, perdiendo así (la iguana) su relevancia en el juego.
+                </li>
+                <br>
+                <h5 style="color:var(--prim-ligth);">Segunda prueba</h5>
+                <li style="color:lightblue;">
+                    Ya se tenían armadas las reglas y los personajes, pero aún esas reglas eran suceptibles a cambios debido a que eran demasiado ambigua, a tal punto que nuestro equipo
+                    no sabía como reaccionar a ciertos eventos.
+                </li>
+                <li style="color:lightblue;">
+                    Cómo el jugador de la iguana se podía mover y debía esperar a que robaran el huevo para moverse.
+                </li>
+                <br>
+                <h5 style="color:var(--prim-ligth);">Tercera prueba</h5>
+                <li style="color:lightblue;">
+                    El jugador de la iguana se movía muy lento, ya sea con las miniiguanas o con la iguana legendaria.
+                </li>
+                <li style="color:lightblue;">
+                    Las reglas para el rol de la iguana no eran muy claras.
+                </li> 
             </div>
             <br>
             <h3 class="text-white title-bg iguana-title">Retroalimentación y Cambios</h3>
-            <div class="puntos-clave" style="padding: 1vh; margin: 1vh;>
+            <div class="puntos-clave" style="padding: 1vh; margin: 1vh;">
                 <div class="linea">
                     <p style="color:lightblue;">
                         Observando los evidentes problemas, agregamos una flecha en la parte superior izquierda del tablero para indicar por donde deben comenzar
@@ -85,10 +81,13 @@ function puntoF(){
                         Muy aparte de la parte visual, cambiamos la funcionalidad de la iguana, en vez de que sea un <b>NPC</b>, lo pasamos a que sea un personaje jugable. 
                         Habían partes de las reglas que no eran muy claras y hacían falta agregar otras para poder aclarar muchos puntos, entre esos la jugabilidad del nuevo 
                         personaje de la iguana.
+                        <br>
+                        Además, se implementaron a los nuevos personajes para el jugador de la iguana, los cuáles son las miniiguanas. Mientras la iguana principal permanece
+                        esperando, este jugador podrá mover las miniiguanas para atrapar a los demás jugadores (ladrones).
                     </p>
                 </div>
             </div>
         </div>
-    </section>    
+    </section>
     `;
 }
